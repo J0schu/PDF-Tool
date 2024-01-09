93052b4f390a82f02492ed1410f7472542e20c0c
J0schu <Joschu2015@t-online.de>
2024-01-09 22:35:48 +0100

    writing metadata

--- pdf.py
+++ pdf.py
@@ -1,8 +1,41 @@
-from PyPDF2 import PdfMerger
+from datetime import datetime
+from PyPDF2 import PdfMerger, PdfFileReader
+import os
+import pwd
+
+time = datetime.utcnow().strftime(f"D\072%Y%m%d%H%M%S")
 
 def merger(pdfs, output_name):
     merger = PdfMerger()
     for pdf in pdfs:
         merger.append(pdf)
+    merger.add_metadata(
+        {
+        "/Author": pwd.getpwuid(os.getuid())[4],
+        "/Producer": "PDF-Tool",
+        "/CreationDate" : time,
+        "/ModTime": time,
+        }
+    )
     merger.write(output_name)
     merger.close()
+
+def get_author(filename):
+    reader = PdfFileReader(filename)
+    meta = reader.metadata
+    return meta.author
+
+def get_creator(filename):
+    reader = PdfFileReader(filename)
+    meta = reader.metadata
+    return meta.creator
+
+def get_subject(filename):
+    reader = PdfFileReader(filename)
+    meta = reader.metadata
+    return meta.subject
+
+def get_title(filename):
+    reader = PdfFileReader(filename)
+    meta = reader.metadata
+    return meta.title
