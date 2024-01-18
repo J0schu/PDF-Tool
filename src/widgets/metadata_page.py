99723645100827b7421af1ca86414df0e3d0d8ce
J0schu <Joschu2015@t-online.de>
2024-01-18 18:28:40 +0100

    write_metadata button

--- src/widgets/metadata_page.py
+++ src/widgets/metadata_page.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QWidget, QListWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QMessageBox, QFormLayout
-from functions.pdf import get_author, get_creator, get_subject, get_title
+from functions.pdf import get_author, get_creator, get_subject, get_title, add_metadata
 
 class MetadataPage(QWidget):
     def __init__(self):
@@ -21,14 +21,25 @@ class MetadataPage(QWidget):
         title_label = QLabel("Title: ")
         self.title_line_edit = QLineEdit()
 
-        layout = QFormLayout()
-        layout.addRow(metadata_label_input, self.metadata_line_edit_input)
-        layout.addRow(author_label, self.author_line_edit)
-        layout.addRow(creator_label, self.creator_line_edit)
-        layout.addRow(subject_label, self.subject_line_edit)
-        layout.addRow(title_label, self.title_line_edit)
+        button_write_metadate = QPushButton("Write Metadata")
+        button_write_metadate.clicked.connect(self.button_write_metadate_clicked)
 
-        self.setLayout(layout)
+        formlayout= QFormLayout()
+        formlayout.addRow(metadata_label_input, self.metadata_line_edit_input)
+        formlayout.addRow(author_label, self.author_line_edit)
+        formlayout.addRow(creator_label, self.creator_line_edit)
+        formlayout.addRow(subject_label, self.subject_line_edit)
+        formlayout.addRow(title_label, self.title_line_edit)
+
+        vbox1 = QVBoxLayout()
+        vbox1.addLayout(formlayout)
+        vbox2 = QVBoxLayout()
+        vbox2.addWidget(button_write_metadate)
+        vbox3 = QVBoxLayout()
+        vbox3.addLayout(vbox1)
+        vbox3.addLayout(vbox2)
+
+        self.setLayout(vbox3)
 
 
     
@@ -39,3 +50,6 @@ class MetadataPage(QWidget):
         self.creator_line_edit.setText(get_creator(self.metadata_line_edit_input.text() + ".pdf"))
         self.subject_line_edit.setText(get_subject(self.metadata_line_edit_input.text() + ".pdf"))
         self.title_line_edit.setText(get_title(self.metadata_line_edit_input.text() + ".pdf"))
+
+    def button_write_metadate_clicked(self):
+        add_metadata(self.metadata_line_edit_input.text() + ".pdf", self.author_line_edit.text(), self.creator_line_edit.text(), self.subject_line_edit.text(), self.title_line_edit.text())
