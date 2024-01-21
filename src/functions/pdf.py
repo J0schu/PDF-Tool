3cfb31306e64ac5da71d1469e8c57f2eda58cf2d
J0schu <Joschu2015@t-online.de>
2024-01-21 20:35:12 +0100

    metadata_edit fixed

--- src/functions/pdf.py
+++ src/functions/pdf.py
@@ -51,7 +51,10 @@ def add_keywords(filename, keywords):
         writer.write(f)
 
 def add_metadata(filename, author, creator, subject, title):
+    reader = PdfReader(filename)
     writer = PdfWriter()
+    for page in reader.pages:
+        writer.add_page(page)
     writer.add_metadata(
         {
         "/Author": author,
